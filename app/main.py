0219978e40c9c599d28dea2fb59cab56eb280dbf
maksym <kapkomaxim@gmail.com>
2026-05-25 17:47:51 +0300

    chore: add CORS middleware for frontend integration

--- app/main.py
+++ app/main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.middleware.cors import CORSMiddleware
+
 from app.core.db_helper import db_helper
 
 app = FastAPI(
     title="Coriolis Trading Terminal",
     version="0.1.0"
+)
+
+origins = [
+    "http://localhost:5173",  # Дефолтный порт Vite
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
