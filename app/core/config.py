f56c65adc48f7d900e3aeae83ac5edb9abf00f56
maksym <kapkomaxim@gmail.com>
2026-05-28 15:02:37 +0300

    Add nado_network: str = testnet to config

--- app/core/config.py
+++ app/core/config.py
@@ -12,6 +12,7 @@ class Settings(BaseSettings):
     privy_app_id: str = ""
     privy_app_secret: str = ""
     api_v1_prefix: str = "/api/v1"
+    nado_network: str = "testnet"
 
     model_config = SettingsConfigDict(
         env_file=BASE_DIR / ".env", env_file_encoding="utf-8", extra="ignore"
