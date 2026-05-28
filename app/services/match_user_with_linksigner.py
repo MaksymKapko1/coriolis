7ab9a808d146267d8b8ec1973bbdabbc0b7a8799
maksym <kapkomaxim@gmail.com>
2026-05-28 13:52:49 +0300

    match user with linksgner added, takes user address and decrypts pk for trading

---
+++ app/services/match_user_with_linksigner.py
@@ -0,0 +1,49 @@
+import logging
+
+from sqlmodel import select
+from sqlmodel.ext.asyncio.session import AsyncSession
+from starlette import status
+from starlette.exceptions import HTTPException
+
+from app.core.security import crypto_manager
+from app.models.user import User
+
+logger = logging.getLogger(__name__)
+
+
+async def get_subaccount_and_signer(
+    main_wallet: str, db_session: AsyncSession
+) -> tuple[str, str]:
+    stmt = select(User).where(User.address == main_wallet)
+    result = await db_session.exec(stmt)
+    user = result.one_or_none()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with {main_wallet} hasn't been linked yet.",
+        )
+
+    if not user.is_approved:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Access restricted, user is not approved.",
+        )
+
+    if not user.encrypted_linked_signer_key:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="User does not have linked signer key",
+        )
+
+    try:
+        decrypted_private_key = crypto_manager.decrypt_private_key(
+            user.encrypted_linked_signer_key
+        )
+    except ValueError:
+        logger.error("Failed to decrypt linked signer key for wallet %s", main_wallet)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error while decrypting linked signer key",
+        )
+    return user.linked_signer_address, decrypted_private_key
